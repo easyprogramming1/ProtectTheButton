1f2b69f10f55530a10cced4062bbbaaea76ebc16
Leo <leomatssonhuovinen@gmail.com>
2025-05-26 13:02:42 +0200

    turret

---
+++ The button/Assets/Turretbullet.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Turretbullet : MonoBehaviour
+{
+    public Rigidbody2D rig;
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.linearVelocity = transform.up * speed;
+    }
+}
--- The button/Assets/theButtonScript.cs
+++ The button/Assets/theButtonScript.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -6,6 +7,10 @@ using UnityEngine.InputSystem;
 public class theButtonScript : MonoBehaviour
 {
     public bool click;
+    public GameObject plott1;
+    public GameObject plott2;
+    public GameObject plott3;
+    public GameObject plott4;
     void Update()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
