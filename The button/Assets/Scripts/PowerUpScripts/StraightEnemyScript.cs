22e0a0cac6889571bc78348cb53ff06892a010fe
Leo <leomatssonhuovinen@gmail.com>
2025-05-27 21:05:05 +0200

    dificultyScale

--- The button/Assets/Scripts/PowerUpScripts/StraightEnemyScript.cs
+++ The button/Assets/Scripts/PowerUpScripts/StraightEnemyScript.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.GraphicsBuffer;
@@ -6,12 +9,15 @@ public class StraightEnemyScript : MonoBehaviour
 {
     public Transform button;
     public float speed;
+    public bool bigenemy;
+    public float hp;
+    Rigidbody2D eig;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         button = GameObject.Find("The Button").transform;
-
+        eig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -36,8 +42,7 @@ public class StraightEnemyScript : MonoBehaviour
 
             if (hit.collider != null && hit.collider.gameObject == gameObject)
             {
-                button.BroadcastMessage("AddCoin");
-                Destroy(gameObject);
+                hp -= 1;
 
             }
         }
@@ -46,8 +51,20 @@ public class StraightEnemyScript : MonoBehaviour
         {
             transform.name = closestEnemy.transform.name + "e";
         }
-       
+        if(hp <= 0)
+        {
+            if (bigenemy)
+            {
+                button.BroadcastMessage("AddCoinBig");
+            }
+            else
+            {
+                button.BroadcastMessage("AddCoin");
+            }
+            Destroy(gameObject);
+        }
     }
+    
     GameObject VoidClosestEnemy()
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("point");
@@ -72,13 +89,11 @@ public class StraightEnemyScript : MonoBehaviour
     {
         if(collision.transform.tag == "bullet")
         {
-            button.BroadcastMessage("AddCoin");
-            Destroy(gameObject);
+            hp -= 1;
         }
         if (collision.transform.tag == "lazer")
         {
-            button.BroadcastMessage("AddCoin");
-            Destroy(gameObject);
+            hp -= 1;
         }
         if (collision.transform.tag == "Button")
         {
@@ -86,5 +101,6 @@ public class StraightEnemyScript : MonoBehaviour
         }
 
     }
+    
 
 }
